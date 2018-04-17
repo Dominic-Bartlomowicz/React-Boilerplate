44ba1a87a1f77e5d33b8e6c0b0a8f35c0934ad04
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-04-16 17:35:48 -0700

    Day 1 Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
+
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import MessageList from './MessageList.jsx';
+import Message from './Message.jsx';
 
 class App extends Component {
+
   render() {
+    console.log("Rendering <App/>");
     return (
     <div>
-<nav className="navbar">
-<a href="/" className="navbar-brand">Chatty</a>
-</nav>
-<main className="messages">
-<div className="message">
-  <span className="message-username">Anonymous1</span>
-  <span className="message-content">I won't be impressed with technology until I can download food.</span>
-</div>
-<div className="message system">
-  Anonymous1 changed their name to nomnom.
-</div>
-</main>
-<footer className="chatbar">
-<input className="chatbar-username" placeholder="Your Name (Optional)" />
-<input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-</footer>
-</div>
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+      </nav>
+      <ChatBar/>
+      <MessageList/>
+      <Message/>
+    </div>
     );
   }
 }
+
 export default App;
